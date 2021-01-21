36512d4c9d36e75353403718a24ec89e64df9653
karimadnan <karimmadnan@gmail.com>
2021-01-21 18:53:11 +0200

    Navbar fixes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,49 +7,54 @@ import Rotworm from "../imgs/rotworm.gif";
 import Orc from "../imgs/orc.gif";
 import Dragon from "../imgs/dragon.gif";
 import Mummy from "../imgs/mummy.gif";
+import Demon from "../imgs/demon.gif";
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    maxHeight: 80,
+    minHeight: 60,
+    maxHeight: 60,
+
   },
   Logo: {
     width: 30,
     height: 30,
     marginRight: theme.spacing(1),
-    marginBottom: theme.spacing(3),
+    marginBottom: theme.spacing(1),
     [theme.breakpoints.up("sm")]: {
       width: 60,
       height: 60,
-      marginRight: theme.spacing(3),
-      marginBottom: theme.spacing(3),
+      marginRight: theme.spacing(1),
+      marginBottom: theme.spacing(1),
     },
   },
   title: {
     flexGrow: 1,
     fontSize: 39,
-    display: "block",
+    display: "flex",
+    margin: theme.spacing(1),
+    justifyContent: 'center',
     fontFamily: "martel",
   },
 }));
 
 export default function SearchAppBar() {
   const classes = useStyles();
-  const monsters = [Rotworm, Mummy, Dragon, Orc]
+  const monsters = [Rotworm, Mummy, Dragon, Orc, Demon]
   const randomMonster = monsters[Math.floor(Math.random() * monsters.length)];
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
+          <img className={classes.Logo} src={randomMonster} alt="Logo" />
           <Typography className={classes.title} variant="h4" noWrap>
-            <img className={classes.Logo} src={randomMonster} alt="Logo" />
             <Link to={"/"} style={{ textDecoration: "none", color: 'yellow' }}>
               Kimox Scripts
             </Link>
-            <img className={classes.Logo} src={randomMonster} alt="Logo" />
           </Typography>
+          <img className={classes.Logo} src={randomMonster} alt="Logo" />
         </Toolbar>
       </AppBar>
     </div>
